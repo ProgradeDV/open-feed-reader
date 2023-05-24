7868e81cecab0168b997afa5d72aace8f43035fd
Timin8er <botballmaster@hotmail.com>
2023-05-24 18:40:02 -0400

    fixed edit source form's css classes

--- app/site_base/forms.py
+++ app/site_base/forms.py
@@ -10,10 +10,10 @@ class SourceSearchForm(forms.Form):
 
 class EditSourceForm(forms.Form):
     """form for creating a new source or edit existing source"""
-    name = forms.CharField(max_length=255, required=False)
-    site_url = forms.URLField(max_length=255, required=False)
-    feed_url = forms.URLField(max_length=512)
-    image_url = forms.URLField(max_length=512, required=False)
+    name = forms.CharField(max_length=255, required=False, widget=forms.TextInput(attrs={'class': "form-control"}))
+    site_url = forms.URLField(max_length=255, required=False, widget=forms.URLInput(attrs={'class': "form-control"}))
+    feed_url = forms.URLField(max_length=512, widget=forms.URLInput(attrs={'class': "form-control"}))
+    image_url = forms.URLField(max_length=512, required=False, widget=forms.URLInput(attrs={'class': "form-control"}))
 
-    description = forms.CharField(required=False)
-    interval = forms.IntegerField(min_value=0)
+    description = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': "form-control"}))
+    interval = forms.IntegerField(min_value=0, initial=100, widget=forms.NumberInput(attrs={'class': "form-control"}))
