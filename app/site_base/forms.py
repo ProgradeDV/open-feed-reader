613810f2382f6daf18a71641f539e38d83491567
Timin8er <botballmaster@hotmail.com>
2023-08-02 15:38:33 -0400

    removed 'interval' field from source form

--- app/site_base/forms.py
+++ app/site_base/forms.py
@@ -16,4 +16,3 @@ class EditSourceForm(forms.Form):
     image_url = forms.URLField(max_length=512, required=False, widget=forms.URLInput(attrs={'class': "form-control"}))
 
     description = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': "form-control"}))
-    interval = forms.IntegerField(min_value=0, initial=100, widget=forms.NumberInput(attrs={'class': "form-control"}))
