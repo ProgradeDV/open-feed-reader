450dee9a696a8a5d9ab1bc352a4534d9410653ee
Timin8er <botballmaster@hotmail.com>
2025-02-16 01:23:53 -0500

    updated the postgress and python versions

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11
+FROM python:3.13
 
 # clean up the python logging
 ENV PYTHONBUFFERED=1
--- app/open_feed_reader/settings.py
+++ app/open_feed_reader/settings.py
@@ -130,8 +130,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/static'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # user login session
