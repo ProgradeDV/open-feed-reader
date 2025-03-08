b63f024b24ef883644f6342bdb2ba3ce1cbdbd1d
Timin8er <botballmaster@hotmail.com>
2025-03-07 22:35:11 -0500

    fixed search input

--- app/site_base/static/site_base/config.js
+++ app/site_base/static/site_base/config.js
@@ -1,1 +1,5 @@
-htmx.config.includeIndicatorStyles = false
+if (!(typeof myVar === 'undefined')) {
+    htmx.config.includeIndicatorStyles = false
+    htmx.config.allowEval = false
+    htmx.config.selfRequestsOnly = false
+}
