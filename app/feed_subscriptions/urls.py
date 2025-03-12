219398e01fd28c7d7e518c934dbcedde861222f4
Timin8er <botballmaster@hotmail.com>
2025-03-11 21:30:10 -0400

    more renaming source to feed

--- app/feed_subscriptions/urls.py
+++ app/feed_subscriptions/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('feeds/',                      views.all_feeds,          name='all_feeds'),
     path('feeds/search/',               views.all_feeds_search,   name='feeds_search'),
     path('feeds/<int:id>/',             views.one_feed,           name='one_feed'),
-    path('feeds/<int:id>/subscribe/',   views.subscribe_feed,     name='subscribe_source'),
-    path('feeds/<int:id>/unsubscribe/', views.unsubscribe_feed,   name='unsubscribe_source'),
+    path('feeds/<int:id>/subscribe/',   views.subscribe_feed,     name='subscribe_feed'),
+    path('feeds/<int:id>/unsubscribe/', views.unsubscribe_feed,   name='unsubscribe_feed'),
     path('subscriptions/',              views.user_subscriptions, name='subscriptions'),
 ]
