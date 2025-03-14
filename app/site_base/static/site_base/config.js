3eb2f2b4c390bed829c39f62b72469622dd3af29
Timin8er <botballmaster@hotmail.com>
2025-03-13 20:29:30 -0400

    added json for tooltip compatability

--- app/site_base/static/site_base/config.js
+++ app/site_base/static/site_base/config.js
@@ -1,5 +1,10 @@
+// config for htmx
 if (!(typeof myVar === 'undefined')) {
     htmx.config.includeIndicatorStyles = false
     htmx.config.allowEval = false
     htmx.config.selfRequestsOnly = false
 }
+
+// enable bootstrap popups
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
