2d824806738515e1e24979707821764b5c901e0d
Timin8er <botballmaster@hotmail.com>
2025-03-19 11:28:59 -0400

    formatting

--- app/feeds_folders/urls.py
+++ app/feeds_folders/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new/',           views.create_folder, name='create_folder'),
-    path('<int:id>/',      views.folder_page,   name='folder_page'),
-    path('<int:id>/edit/',                views.edit_folder,   name='edit_folder'),
-    path('<int:id>/add/<int:feed_id>',    views.remove_feed_from_folder,   name='remove_feed_from_folder'),
-    path('<int:id>/remove/<int:feed_id>', views.add_feed_to_folder,   name='add_feed_to_folder'),
+    path('new/',                          views.create_folder,           name='create_folder'),
+    path('<int:id>/',                     views.folder_page,             name='folder_page'),
+    path('<int:id>/edit/',                views.edit_folder,             name='edit_folder'),
+    path('<int:id>/add/<int:feed_id>',    views.remove_feed_from_folder, name='remove_feed_from_folder'),
+    path('<int:id>/remove/<int:feed_id>', views.add_feed_to_folder,      name='add_feed_to_folder'),
 ]
