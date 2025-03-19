23f230b475d78ecd62bc0ee8305e7648415426cd
Timin8er <botballmaster@hotmail.com>
2025-03-18 23:55:37 -0400

    system to add/remove sources from folders

--- app/folders/views.py
+++ app/folders/views.py
@@ -1,9 +1,11 @@
 from logging import getLogger
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render, HttpResponse
+from feeds.models import Source
 
 from .models import SourcesFolder
 
+logger=getLogger('folders')
 
 @login_required
 def folder_page(request: HttpResponse, id: int):
@@ -43,10 +45,56 @@ def edit_folder(request: HttpResponse, id:int):
     except SourcesFolder.DoesNotExist:
         return None
 
+    # all_feeds = Source.objects.all()
+    all_feeds = Source.objects.all().order_by('name')
+    ordered_feeds = sorted(all_feeds, key=lambda f: (f not in folder.sources.all()))
+
     return render(
         request,
         'folders/edit_folder.html',
         context={
-            "folder":folder
+            "folder":folder,
+            "all_feeds":ordered_feeds,
             },
         )
+
+
+@login_required
+def add_feed_to_folder(request: HttpResponse, id:int, feed_id:int):
+    """add a feed to a folder"""
+    # reject non post requests
+    if request.method != "POST":
+        return None
+
+    # reject subscriptions to sources that don't exist
+    try:
+        feed = Source.objects.get(id = feed_id)
+    except Source.DoesNotExist:
+        return None
+
+    folder = SourcesFolder.objects.get(id=id)
+    folder.sources.add(feed)
+    folder.save()
+
+    # return an unsubscribe button
+    return render(request, 'folders/feed_list_item.html', context={'folder':folder, 'feed':feed})
+
+
+def remove_feed_from_folder(request: HttpResponse, id:int, feed_id:int):
+    """remove a feed from a folder"""
+    # reject non post requests
+    if request.method != "POST":
+        return None
+
+    # reject subscriptions to sources that don't exist
+    try:
+        feed = Source.objects.get(id = feed_id)
+    except Source.DoesNotExist:
+        return None
+
+    folder = SourcesFolder.objects.get(id=id)
+    folder.sources.remove(feed)
+    folder.save()
+
+    # return an unsubscribe button
+    return render(request, 'folders/feed_list_item.html', context={'folder':folder, 'feed':feed})
