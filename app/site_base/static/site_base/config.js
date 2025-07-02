2d9366d0a5875f572110cb7bbb8f58a3e9409c19
Timin8er <botballmaster@hotmail.com>
2025-07-02 02:38:23 -0400

    removed unnessisary js

--- app/site_base/static/site_base/config.js
+++ app/site_base/static/site_base/config.js
@@ -8,7 +8,3 @@ if (!(typeof myVar === 'undefined')) {
 // enable bootstrap popups
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-$(document).ready(function(){
-  $('.toast').toast('show');
-});
