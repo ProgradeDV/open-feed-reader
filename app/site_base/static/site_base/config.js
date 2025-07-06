6bf4b1afc0936a46158e5415701e9d19597cde10
Timin8er <botballmaster@hotmail.com>
2025-07-05 22:01:46 -0400

    added tooltips to sub/unsub buttons

--- app/site_base/static/site_base/config.js
+++ app/site_base/static/site_base/config.js
@@ -1,10 +1,16 @@
 // config for htmx
+// enable bootstrap popups
+function enable_all_tooltips() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+}
+enable_all_tooltips()
+// listen for events to re enable all tooltips
+document.body.addEventListener('htmx:afterRequest', enable_all_tooltips)
+
+
 if (!(typeof htmx === 'undefined')) {
     htmx.config.includeIndicatorStyles = false
     htmx.config.allowEval = false
     htmx.config.selfRequestsOnly = true
 }
-
-// enable bootstrap popups
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
