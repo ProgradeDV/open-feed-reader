16a4ffaf9baa4a3b76cc43026825f37ba727bee3
Timin8er <botballmaster@hotmail.com>
2025-07-10 22:33:44 -0400

    added not found error to entry and feed

--- app/feeds/views.py
+++ app/feeds/views.py
@@ -2,6 +2,7 @@
 import logging
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponse
+from django.http import Http404
 from site_base.views import paginator_args
 from feeds.models import Source, Entry
 
@@ -11,7 +12,11 @@ logger = logging.getLogger("feed_management")
 @login_required
 def feed_page(request: HttpResponse, id: int):
     """the view for a single feed and it's entries"""
-    feed = Source.objects.get(id=id)
+    try:
+        feed = Source.objects.get(id=id)
+    except Entry.DoesNotExist:
+        raise Http404("Feed Not Found")
+
     entries = Entry.objects.filter(source = feed).order_by('-created')
     is_subed = feed.subscribers.filter(id=request.user.id).exists()
 
@@ -30,7 +35,11 @@ def feed_page(request: HttpResponse, id: int):
 @login_required
 def entry_page(request: HttpResponse, entry_id: int):
     """view for a single entry"""
-    entry = Entry.objects.get(id=entry_id)
+    try:
+        entry = Entry.objects.get(id=entry_id)
+    except Entry.DoesNotExist:
+        raise Http404("Entry Not Found")
+
     return render(request,
         'entries/entry_page.html',
         context={
